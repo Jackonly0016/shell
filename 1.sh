8c0cd39378e954195f618f62174f18e51264c2ba
jack <69616395+Jackonly0016@users.noreply.github.com>
2022-09-27 23:00:17 +0800

    Add files via upload

---
+++ 1.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+echo `ls $1`
+
+function CopyFileToTargetFolder() {
+    for file in `ls $1`
+	do
+		if [ -d $1"/"$file ];then
+	        CopyFileToTargetFolder $1"/"$file
+        elif [ -f $1"/"$file ];then		
+	        str=1"/"$file
+	        if [ "${str##*.}"x = "so"x ];then
+	            cp $1"/"$file /home/jack/learning/shell/target #目标路径
+	            echo $1"/"$file
+            fi
+	    fi
+    done
+}
+
+SourcePath='/home/jack/learning/shell/src' #源路径
+CopyFileToTargetFolder $SourcePath  
+
