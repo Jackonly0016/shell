692f4d234f1224eef60633c7e0f7cbeb025a3d98
jack <69616395+Jackonly0016@users.noreply.github.com>
2022-09-27 23:01:50 +0800

    Update 1.sh

--- 1.sh
+++ 1.sh
@@ -5,7 +5,7 @@ echo `ls $1`
 function CopyFileToTargetFolder() {
     for file in `ls $1`
 	do
-		if [ -d $1"/"$file ];then
+	    if [ -d $1"/"$file ];then
 	        CopyFileToTargetFolder $1"/"$file
         elif [ -f $1"/"$file ];then		
 	        str=1"/"$file
